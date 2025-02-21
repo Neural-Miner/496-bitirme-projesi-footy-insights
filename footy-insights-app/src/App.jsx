84b61ef25d4307f4e702e9cc9f1f55d287e903db
kaanzsoy <hkaanozsoy@gmail.com>
2025-02-21 23:35:03 +0300

    Arayuzde gelistirmeler, mac verilerinde syntax duzenlemesi

--- footy-insights-app/src/App.jsx
+++ footy-insights-app/src/App.jsx
@@ -3,8 +3,13 @@ import React, { useState, useRef, useEffect } from 'react';
 // import videoBg from './assets/background_video_2.mp4'
 import videoBg from './assets/background_video.mp4'
 import matchesData from './components/matches.json'; // JSON dosyanızı içe aktarın
+//import matchDetailsData from './mac_verileri/2017-2018/2017-2018_1_BEŞİKTAŞ A.Ş._ANTALYASPOR A.Ş..json'; // ornek mac detaylari
+import matchDetailsData from './components/mac_verileri/2018-2019/2018-2019_10_FENERBAHÇE A.Ş._MKE ANKARAGÜCÜ.json'
+
+import MatchDetails from './MatchDetails';
 
 import { FaPlay, FaPause } from "react-icons/fa"; // oynat-duraklat ikonlari
+import { SlArrowLeft } from "react-icons/sl";
 
 function App() {
 
@@ -21,11 +26,10 @@ function App() {
     }
   };
 
-
   return (
     <div className="App">
       <div className="overlay"></div>
-        <video src={videoBg} autoPlay loop muted />
+        <video ref={videoRef} src={videoBg} autoPlay loop muted />
           <div className="content">
             <UploadBox />
           </div>
@@ -49,6 +53,8 @@ const UploadBox = () => {
   const [selectedWeek, setSelectedWeek] = useState('');
   const [matchList, setMatchList] = useState([]);
   const [selectedMatch, setSelectedMatch] = useState('');
+  const [showDetails, setShowDetails] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);  // Form'un kaybolma animasyonu icin
 
   const seasonWeekLimits = Object.fromEntries(
     [...Array(9)].map((_, i) => [`${2011 + i}-${2012 + i}`, 34])
@@ -66,10 +72,14 @@ const UploadBox = () => {
     setSelectedWeek('');
     setMatchList([]);
     setSelectedMatch('');
+    setShowDetails(false);
+    setFadeOut(false);
   };
 
   const handleWeekChange = (e) => {
     setSelectedWeek(e.target.value);
+    setShowDetails(false);
+    setFadeOut(false);
   };
 
   // Sezon ve hafta secimi sonrasi maclari listele
@@ -81,106 +91,91 @@ const UploadBox = () => {
     }
   }, [selectedSeason, selectedWeek]);
 
+  const handleContinue = () => {
+    // Form kaybolma animasyonunu baslat
+    setFadeOut(true);
+
+    // Animasyon suresi (0.5s) bittikten sonra formu DOM'dan kaldirip detaylari goster
+    setTimeout(() => {
+      setShowDetails(true);
+    }, 500); // CSS'teki fadeOutScale suresine esitleyin
+  };
+
   return (
     <div className="container">
+      {/* Form Kapsayici */}
+      {!showDetails && (
+        <div className={`formWrapper ${fadeOut ? 'fadeOutScale' : 'fadeInScale'}`}>
+          <div className="form-group">
+            <label className="label" htmlFor="season">Sezon Seç:</label>
+            <select
+              id="season"
+              className="dropdown"
+              value={selectedSeason}
+              onChange={handleSeasonChange}
+            >
+              <option value="">Sezon Seçiniz</option>
+              {Object.keys(seasonWeekLimits).map((season) => (
+                <option key={season} value={season}>{season}</option>
+              ))}
+            </select>
+          </div>
 
-      <div className="form-group">
-        <label className="label" htmlFor="season">Sezon Seç:</label>
-        <select id="season" className="dropdown" value={selectedSeason} onChange={handleSeasonChange}>
-          <option value="">Sezon Seçiniz</option>
-          {Object.keys(seasonWeekLimits).map((season) => (
-            <option key={season} value={season}>{season}</option>
-          ))}
-        </select>
-      </div>
+          {selectedSeason && (
+            <div className="form-group">
+              <label className="label" htmlFor="week">Hafta Seç:</label>
+              <select
+                id="week"
+                className="dropdown"
+                value={selectedWeek}
+                onChange={handleWeekChange}
+              >
+                <option value="">Hafta Seçiniz</option>
+                {[...Array(seasonWeekLimits[selectedSeason])].map((_, index) => (
+                  <option key={index + 1} value={index + 1}>
+                    {index + 1}. Hafta
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
 
-      {selectedSeason && (
-        <div className="form-group">
-          <label className="label" htmlFor="week">Hafta Seç:</label>
-          <select id="week" className="dropdown" value={selectedWeek} onChange={handleWeekChange}>
-            <option value="">Hafta Seçiniz</option>
-            {[...Array(seasonWeekLimits[selectedSeason])].map((_, index) => (
-              <option key={index + 1} value={index + 1}>{index + 1}. Hafta</option>
-            ))}
-          </select>
+          {selectedWeek && matchList.length > 0 && (
+            <>
+              <label className="label">Maç Seç:</label>
+              <select
+                className="dropdown"
+                value={selectedMatch}
+                onChange={(e) => setSelectedMatch(e.target.value)}
+              >
+                <option value="">Maç Seçiniz</option>
+                {matchList.map((match, index) => (
+                  <option
+                    key={index}
+                    value={`${match.homeTeam} ${match.homeScore}-${match.awayScore} ${match.awayTeam}`}
+                  >
+                    {match.homeTeam} | {match.homeScore}-{match.awayScore} | {match.awayTeam}
+                  </option>
+                ))}
+              </select>
+            </>
+          )}
+
+          {selectedMatch && (
+            <button onClick={handleContinue}>Devam</button>
+          )}
         </div>
       )}
 
-      {selectedWeek && matchList.length > 0 && (
-        <>
-          <label className="label">Maç Seç:</label>
-          <select className="dropdown" value={selectedMatch} onChange={(e) => setSelectedMatch(e.target.value)}>
-            <option value="">Maç Seçiniz</option>
-            {matchList.map((match, index) => (
-              <option key={index} value={`${match.homeTeam} ${match.homeScore}-${match.awayScore} ${match.awayTeam}`}>
-                {match.homeTeam} | {match.homeScore}-{match.awayScore} | {match.awayTeam}
-              </option>
-            ))}
-          </select>
-        </>
-      )}
-
-      {/*
-      {selectedMatch && (
-        <div className="selected-match">
-          <h3>Seçilen Maç:</h3>
-          <p>{selectedMatch}</p>
+      {/* Detay Kapsayici */}
+      {showDetails && (
+        <div className="detailsWrapper fadeInScale">
+          <MatchDetails details={matchDetailsData} />
         </div>
       )}
-      */}
-
-
-
-
-      {/*
-      <div className="form-group">
-          <label className="label" htmlFor="videoUpload">
-            Maç videosunu yükleyiniz:
-          </label>
-          <input type="file" id="video" accept="video/*" className="input" onChange={handleVideoUpload}/>
-      </div>
-      */}
-
-      {/*
-      <div>
-        <label className='label' htmlFor='homeTeam'>
-          Ev Sahibi Takım:
-        </label>
-        <input type='text' id='homeTeam' placeholder='Ev sahibi takımı giriniz' className='input'/>
-      </div>
-
-      <div>
-        <label className='label' htmlFor='awayTeam'>
-          Deplasman Takımı:
-        </label>
-        <input type='text' id='awayTeam' placeholder='Deplasman takımı giriniz' className='input'/>
-      </div>
-      */}
-
-      {/*
-      <div className="form-group">
-          <label className="label" htmlFor="season">
-            Sezon:
-          </label>
-          <input type="text" id="season" placeholder="(örn. 2023-2024)" className="input"
-          />
-      </div>
-
-        <div className="form-group">
-          <label className="label" htmlFor="week">
-            Hafta:
-          </label>
-          <input type="number" id="week" placeholder="(örn. 8)" min={1}
-            className="input"
-          />
-        </div>
-        */}     
-
     </div>
   );
-
-
-
 };
+
 
 export default App;
